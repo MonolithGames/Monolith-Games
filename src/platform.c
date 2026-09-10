ec6908b518c01432ab5ea7eccfec1fa0380f7ff9
MonolithGames <MonolithGames@gmail.com>
2026-09-10 16:40:31 -0400

    hello_world

--- src/platform.c
+++ src/platform.c
@@ -4,7 +4,7 @@
 #include <wingdi.h>
 
 #define TOPBAR_HEIGHT 56
-#define TABBAR_HEIGHT 36
+#define TABBAR_HEIGHT 40
 #define MENU_WIDTH 120
 #define MENU_HEIGHT 40
 #define RELOAD_TIMER 1
@@ -55,8 +55,8 @@ void DrawReloadIcon(HDC hdc, int x, int y, int spin)
 void DrawFluentShadow(HDC hdc, RECT* rect)
 {
     TRIVERTEX vertex[2] = {
-        {0, TOPBAR_HEIGHT, RGB(240,240,240), 0, 0},
-        {rect->right, TOPBAR_HEIGHT + 12, RGB(200,200,200), 0, 0}
+        {0, TOPBAR_HEIGHT + TABBAR_HEIGHT, 0xF000, 0xF000, 0xF000, 0x0000},
+        {rect->right, TOPBAR_HEIGHT + TABBAR_HEIGHT + 12, 0xC000, 0xC000, 0xC000, 0x0000}
     };
 
     GRADIENT_RECT gRect = {0, 1};
@@ -64,41 +64,24 @@ void DrawFluentShadow(HDC hdc, RECT* rect)
 }
 
 // ------------------------------------------------------------
-// Draw Top Bar
+// Draw Acrylic Tab Background
 // ------------------------------------------------------------
-void DrawTopBar(HDC hdc, RECT* rect)
+void DrawAcrylicTab(HDC hdc, RECT* r, int hovered, int active)
 {
-    HFONT font = CreateFont(18, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
-                            DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
-                            DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, "Segoe UI Symbol");
-    SelectObject(hdc, font);
+    HDC memDC = CreateCompatibleDC(hdc);
+    HBITMAP bmp = CreateCompatibleBitmap(hdc, r->right - r->left, r->bottom - r->top);
+    SelectObject(memDC, bmp);
 
-    RECT topbar = {0, 0, rect->right, TOPBAR_HEIGHT};
-    HBRUSH bar = CreateSolidBrush(RGB(240, 240, 240));
-    FillRect(hdc, &topbar, bar);
-    DeleteObject(bar);
+    HBRUSH b = CreateSolidBrush(hovered ? RGB(255,255,255) : RGB(245,245,245));
+    FillRect(memDC, &(RECT){0,0,r->right - r->left, r->bottom - r->top}, b);
+    DeleteObject(b);
 
-    SetBkMode(hdc, TRANSPARENT);
-    SetTextColor(hdc, RGB(40, 40, 40));
+    BLENDFUNCTION blend = {AC_SRC_OVER, 0, active ? 200 : 160, 0};
+    AlphaBlend(hdc, r->left, r->top, r->right - r->left, r->bottom - r->top,
+               memDC, 0, 0, r->right - r->left, r->bottom - r->top, blend);
 
-    TextOut(hdc, 12, 18, "☰", 3);
-
-    reloadRect.left = 48; reloadRect.top = 18;
-    reloadRect.right = 68; reloadRect.bottom = 38;
-    DrawReloadIcon(hdc, reloadRect.left, reloadRect.top, reloadSpin);
-
-    favRect.left = rect->right - 220; favRect.top = 18;
-    accRect.left = rect->right - 180; accRect.top = 18;
-    chatRect.left = rect->right - 260; chatRect.top = 18;
-
-    TextOut(hdc, favRect.left, favRect.top, "★", 3);
-    TextOut(hdc, accRect.left, accRect.top, "👤", 3);
-    TextOut(hdc, chatRect.left, chatRect.top, "💬 Chat", 7);
-
-    dotsRect.left = rect->right - 330; dotsRect.top = 18;
-    TextOut(hdc, dotsRect.left, dotsRect.top, "...", 3);
-
-    DeleteObject(font);
+    DeleteObject(bmp);
+    DeleteDC(memDC);
 }
 
 // ------------------------------------------------------------
@@ -106,37 +89,34 @@ void DrawTopBar(HDC hdc, RECT* rect)
 // ------------------------------------------------------------
 void DrawTabBar(HDC hdc, RECT* rect)
 {
+    RECT strip = {0, TOPBAR_HEIGHT, rect->right, TOPBAR_HEIGHT + TABBAR_HEIGHT};
+    HBRUSH stripBrush = CreateSolidBrush(RGB(255,255,255));
+    FillRect(hdc, &strip, stripBrush);
+    DeleteObject(stripBrush);
+
     HFONT font = CreateFont(16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
                             DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
                             DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, "Segoe UI");
     SelectObject(hdc, font);
 
-    RECT tabbar = {0, TOPBAR_HEIGHT, rect->right, TOPBAR_HEIGHT + TABBAR_HEIGHT};
-    HBRUSH bar = CreateSolidBrush(RGB(250, 250, 250));
-    FillRect(hdc, &tabbar, bar);
-    DeleteObject(bar);
-
     SetBkMode(hdc, TRANSPARENT);
 
     for (int i = 0; i < 3; i++)
     {
-        tabRect[i].left = 20 + i * 100;
-        tabRect[i].top = TOPBAR_HEIGHT + 8;
-        tabRect[i].right = tabRect[i].left + 80;
-        tabRect[i].bottom = TOPBAR_HEIGHT + 28;
+        tabRect[i].left = 20 + i * 120;
+        tabRect[i].top = TOPBAR_HEIGHT + 6;
+        tabRect[i].right = tabRect[i].left + 100;
+        tabRect[i].bottom = TOPBAR_HEIGHT + 34;
 
-        if (hoverIndex == i)
-            SetTextColor(hdc, RGB(0, 120, 215));
-        else
-            SetTextColor(hdc, RGB(40, 40, 40));
+        DrawAcrylicTab(hdc, &tabRect[i], hoverIndex == i, activeTab == i);
 
-        char label[8];
-        wsprintf(label, "Tab %d", i + 1);
-        DrawText(hdc, label, -1, &tabRect[i], DT_SINGLELINE | DT_VCENTER | DT_CENTER);
+        SetTextColor(hdc, RGB(40,40,40));
+        DrawText(hdc, activeTab == i ? "Tab Active" : "Tab", -1, &tabRect[i],
+                 DT_SINGLELINE | DT_VCENTER | DT_CENTER);
 
-        if (i == activeTab)
+        if (activeTab == i)
         {
-            HPEN pen = CreatePen(PS_SOLID, 2, RGB(0, 120, 215));
+            HPEN pen = CreatePen(PS_SOLID, 2, RGB(0,120,215));
             SelectObject(hdc, pen);
             MoveToEx(hdc, tabRect[i].left, tabRect[i].bottom, NULL);
             LineTo(hdc, tabRect[i].right, tabRect[i].bottom);
@@ -227,8 +207,8 @@ LRESULT CALLBACK Platform_WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM 
         DeleteObject(bg);
 
         DrawTopBar(hdc, &rect);
-        DrawFluentShadow(hdc, &rect);
         DrawTabBar(hdc, &rect);
+        DrawFluentShadow(hdc, &rect);
         DrawMenu(hdc);
 
         return 1;
